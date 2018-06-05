d3b2ec3af7c01e2403ffbbcc4e8d901475bc04bf
earowang <earo.wang@gmail.com>
2018-06-05 11:14:58 +1000

    used R/ instead of src/

--- R/project.R
+++ R/project.R
@@ -4,11 +4,16 @@
 #'
 #' @export
 create_project <- function(path) {
-  usethis::create_project(path, rstudio = TRUE, open = FALSE)
+  dir.create(path)
+  path <- normalizePath(path, mustWork = TRUE)
+  usethis::proj_set(path, force = TRUE)
+
+  # create RStudio project
+  usethis::use_rstudio()
   usethis::use_git_ignore(c(".DS_Store"))
 
-  # I put my R code to /src
-  usethis::use_directory("src")
+  # I put my R code to /R
+  usethis::use_directory("R")
   # I put data /data
   usethis::use_directory("data")
 
